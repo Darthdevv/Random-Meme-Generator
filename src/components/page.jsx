ad1ba3ff5cd64a926b707048f7db4e0371903840
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-12-18 01:04:59 +0200

    😂 first commit

---
+++ src/components/page.jsx
@@ -0,0 +1,18 @@
+import Header from './header';
+import Meme from './main';
+
+
+
+
+function Page() {
+    return (
+        <>
+            <Header
+            img="troll-face.png"
+            />
+            <Meme/>
+        </>
+    ) 
+}
+
+export default Page;
